f7bb99ab51862002917715b8ed29452ca2d6049b
Carlos Daniel <csaavedrac@alumno.unsm.edu.pe>
2022-08-21 14:31:22 -0500

    casi todo e bienes raices

--- controllers/appController.js
+++ controllers/appController.js
@@ -41,12 +41,35 @@ const inicio = async (req, res) => {
     })
 }
 
-const categoria = (req, res) => {
+const categoria = async (req, res) => {
+    const { id } = req.params
 
+    // Comprobar que la categoria visita exista
+    const categoria = await Categoria.findByPk(id)
+    if (!categoria) {
+        return res.redirect('/404')
+    }
+    // Obtener las propiedades una vez validado la categoria
+    // si llego hasta aca es porque existe la categoria
+    const propiedades = await Propiedad.findAll({
+        where: {
+            categoriaID: id
+        },
+        include: [
+            { model: Precio, as: 'precio' }
+        ]
+    })
+
+    res.render('categoria', {
+        pagina: `${categoria.nombre}s en Venta`,
+        propiedades,
+    })
 }
 
 const noEncontrado = (req, res) => {
-
+    res.render('404', {
+        
+    })
 }
 
 const buscador = (req, res) => {
