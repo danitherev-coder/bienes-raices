cb827e5cea07e7a508456004976f4d766180a95b
Carlos Daniel <csaavedrac@alumno.unsm.edu.pe>
2022-08-22 13:26:57 -0500

    agregue buscador

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,3 +1,4 @@
+import { Sequelize } from 'sequelize';
 import { Precio, Categoria, Propiedad } from '../models/index.js'
 
 const inicio = async (req, res) => {
@@ -37,7 +38,8 @@ const inicio = async (req, res) => {
         categorias,
         precios,
         casas,
-        departamentos
+        departamentos,
+        csrfToken: req.csrfToken()
     })
 }
 
@@ -63,17 +65,40 @@ const categoria = async (req, res) => {
     res.render('categoria', {
         pagina: `${categoria.nombre}s en Venta`,
         propiedades,
+        csrfToken: req.csrfToken()
     })
 }
 
 const noEncontrado = (req, res) => {
     res.render('404', {
-        
+        csrfToken: req.csrfToken()
     })
 }
 
-const buscador = (req, res) => {
+const buscador = async (req, res) => {
+    const { termino } = req.body
 
+    // validar que el termino no este vacio
+    if (!termino.trim()) {
+        return res.redirect("javascript: history.go(-1)")
+    }
+
+    // consultar las propiedades
+    const propiedades = await Propiedad.findAll({
+        where: {
+            titulo: {
+                [Sequelize.Op.like]: '%' + termino + '%'
+            }
+        },
+        include: [
+            { model: Precio, as: 'precio' }
+        ]
+    })
+    res.render('busqueda', {
+        pagina: 'Resultados de la busqueda',
+        propiedades,
+        csrfToken: req.csrfToken()
+    })
 }
 
 export {
